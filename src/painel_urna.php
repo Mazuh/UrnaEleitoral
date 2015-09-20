a9c3c6f03435785e90a0fc10041463cd40cff705
Yuri Sales <yurihenrique60@hotmail.com>
2015-09-20 00:11:56 -0300

    Painel da urna de votação
    /* Incompleto */

---
+++ src/painel_urna.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+	
+	<head>
+		<meta charset="utf-8">
+		<title> Urna eleitoral </title>
+		<style>
+			body{background: #DCDCDC;}
+
+			#urna{
+				margin: auto;
+				margin-top: 5%;
+				width: 60%;
+				background: white;	
+				height: 500px;
+				border-radius: 1%;
+				box-shadow: 5px 5px 5px black;
+			}
+
+			#tela-urna{
+				position: absolute;
+				width: 35%;
+				background: #696969;	
+				height: 400px;
+				border-radius: 1%;
+				margin: auto;
+				margin-left: 1%;
+				margin-top: 3.5%;
+				box-shadow: 5px 5px 5px black;
+			}
+		
+			#tela-botoes{
+				position: absolute;
+				background: black;
+				width: 21%;	
+				height: 400px;
+				margin: right;
+				margin-left: 37.5%;
+				margin-top: 3.5%;
+				border-radius: 1%;
+				box-shadow: 5px 5px 5px #696969;
+			}
+
+		</style>
+	</head>
+
+	<body>
+		<div id="urna">
+			<div id="tela-urna">
+			</div>
+			<div id="tela-botoes">
+
+			</div>
+		</div>
+	</body>
+
+</html>
