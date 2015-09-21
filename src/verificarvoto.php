cf090a09efe5579541bed2d61cea01f0737bbfcf
Yuri Sales <yurihenrique60@hotmail.com>
2015-09-20 21:02:50 -0300

    Verifica o voto do eleitor

---
+++ src/verificarvoto.php
@@ -0,0 +1,11 @@
+<?php
+	$voto = $_GET['voto'];
+
+	if ($voto == "confirmar") {
+			header("Location: painel_urna.php?voto=$voto&valor=1");
+
+	} else { 
+		header("Location: painel_urna.php?voto=$voto");
+	}
+
+?>
