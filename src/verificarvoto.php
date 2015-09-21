1ad7c094b50e02e75dbabe3d9c5a9b373060a979
Yuri Sales <yurihenrique60@hotmail.com>
2015-09-20 21:03:10 -0300

    Update verificarvoto.php

--- src/verificarvoto.php
+++ src/verificarvoto.php
@@ -2,7 +2,7 @@
 	$voto = $_GET['voto'];
 
 	if ($voto == "confirmar") {
-			header("Location: painel_urna.php?voto=$voto&valor=1");
+		header("Location: painel_urna.php?voto=$voto&valor=1");
 
 	} else { 
 		header("Location: painel_urna.php?voto=$voto");
