282a4d44d2c8dddce362201c31467cbe8fa9dfa8
Marcell Guilherme <marcell-mz@hotmail.com>
2015-09-21 15:53:54 -0300

    Campo de senha pra membros do comitê

--- src/sql/eleicao_gremio.sql
+++ src/sql/eleicao_gremio.sql
@@ -11,8 +11,11 @@ CREATE TABLE IF NOT EXISTS usuario(
 	matricula VARCHAR(14) UNIQUE NOT NULL,
 	chapa_votada INTEGER DEFAULT NULL,
 	momento_voto TIMESTAMP NULL DEFAULT NULL,
-    chapa_pertencente INTEGER DEFAULT NULL,
-    membro_comite BOOL NOT NULL DEFAULT FALSE,
+    	
+    	senha_comite VARCHAR(50) DEFAULT NULL,
+    	
+    	chapa_pertencente INTEGER DEFAULT NULL,
+    	membro_comite BOOL NOT NULL DEFAULT FALSE,
     
     FOREIGN KEY(chapa_votada) REFERENCES chapa(id),
     FOREIGN KEY(chapa_pertencente) REFERENCES chapa(id)
